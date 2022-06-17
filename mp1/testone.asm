84e426cb952c405a520129e8c7b1585f47dd8eca
Bill Yang <hy38@illinois.edu>
2022-06-17 09:43:08 -0500

    mp1 original files

---
+++ mp1/testone.asm
@@ -0,0 +1,6 @@
+	.ORIG x4000
+
+	.STRINGZ "When I was young, I learned the sentence that people used to learn typing: \"The quick brown fox jumped over the lazy dog.\"  If you look carefully, or less than carefully at a correctly-produced histogram, you will notice that it contains all of the letters in the English language.  This aspect gives the sentence its value in teaching typing skills.  Can you type it, I wonder?"
+
+	.END
+
---
+++ mp1/testtwo.asm
@@ -0,0 +1,22 @@
+.ORIG x4000
+
+.STRINGZ "
+*** The entry room of the infamous ECE190 'surreal' game about finishing
+*** your MP2 as an ECE190 student...
+
+Being a bright ECE undergraduate on the prestigious UIUC campus, you have
+decided to enroll in ECE199SJP, subjecting yourself to sleep deprivation 
+and the debilitating effects of 'Engineering humor' in search of a deeper
+understanding of computers and their design.
+
+You stand in the Bardeen Quad.  You stand in the rain.  Recalling that
+standing in the rain can make you sick, you briefly consider trying to 
+get sick instead of finishing tonight's MP, but decide to retain that
+option for later.
+
+You can (1) head into Everitt, (2) wave to the Engineer and enter Grainger,
+or (3) go down to Green Street.
+
+"
+
+.END
